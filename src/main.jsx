2fdea3b6bc1a1f39c853294385e92dce34b2e0fb
Chanmith-2K <vinulchanmith@gmail.com>
2026-05-03 12:15:30 +0530

    Use enrollment insights dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './AppCloudMonthly.jsx'
+import App from './AppCloudInsights.jsx'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
